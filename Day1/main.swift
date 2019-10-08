77a1651f623e925eb429c67b33d55481c84250f8
sachin kumar <sachinmahi>
2019-10-07 21:21:40 -0400

    seriously starting of swift

--- Day1/main.swift
+++ Day1/main.swift
@@ -10,3 +10,67 @@ import Foundation
 
 print("Hello, World!")
 
+
+var str = "Hello, playground"
+//print(str)
+//var s: Int=100
+//print(s)
+
+//let a=200
+
+//print(a)
+
+//var b = 100
+//var c = 200
+//var d = b + c
+
+//var e = ("sum of \(b) and \(c) is \(d)")
+//print(e)
+
+//var g = "😁"
+//print(g)
+//var h = "\(g) and \(e)"
+//print(h)
+
+//var x1 = (1, "sachin","mahi")
+//print(x1)
+//print(x1.0)
+//print(x1.1)
+//print(x1.2)
+
+//var x2 = (collegeid:10, collegename:"lambton college", collegecity:"toronto")
+//print(x2.collegeid)
+//print(x2.collegecity)
+//print(x2.collegename)
+//print(x2.self)
+
+//var (collegeid, collegename, collegecity)=x2
+//print(x2)
+
+//var welcomemessage : Int=100
+//print(welcomemessage)
+
+//var aa = 1...10
+//print(aa)
+
+//for i in -10..<10{
+//  print("simar \(aa)")
+//}
+//for i in stride(from: 10, to: 0, by: -1){
+//    print("simar \(i)")
+//}
+
+//var x0 = Int()
+//print(x0)
+
+//var x00: Int?
+//print(x00)
+
+print(str.lowercased())
+print(str.uppercased())
+str.insert("$", at: str.endIndex)
+print(str)
+print(str.count)
+print(str.self)
+print(str.endIndex)
+
